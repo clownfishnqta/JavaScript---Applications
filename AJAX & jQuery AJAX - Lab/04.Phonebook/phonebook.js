c65f0dd8295a47e644b8a9e9f27a09520be2e88b
Ivaylo Plashkov <n3k0ne@gmail.com>
2018-03-23 20:37:28 +0200

    First commit

---
+++ AJAX & jQuery AJAX - Lab/04.Phonebook/phonebook.js
@@ -0,0 +1,61 @@
+const URL = 'https://phonebook-1d3f5.firebaseio.com/phonebook';
+
+$('#btnLoad').on('click', loadData);
+$('#btnCreate').on('click', postData);
+
+function loadData() {
+    $.ajax({
+        method: 'GET',
+        url: URL + '.json'
+    }).then(loadSuccess)
+        .catch(loadError);
+
+    function loadSuccess(res) {
+        $('#phonebook').empty();
+        for (let key in res) {
+            if (res[key] !== null) {
+                let li = $(`<li>${res[key].name}: ${res[key].phone} </li>`).append($('<a href="#">[Delete]</a>')
+                    .click(function () {
+                        $.ajax({
+                            method: 'DELETE',
+                            url: URL + '/' + key + '.json'
+                        }).then(() => $(li).remove())
+                            .catch(loadError);
+                    }));
+                $('#phonebook').append(li);
+            }
+        }
+    }
+}
+
+function postData() {
+    let name = $('#person').val();
+    let phone = $('#phone').val();
+    let postDataJSON = JSON.stringify({name, phone});
+
+    $.ajax({
+        method: 'POST',
+        data: postDataJSON,
+        url: URL + '.json'
+    }).then(createElement)
+        .catch(loadError);
+    function createElement() {
+        let li = $(`<li>${name}: ${phone} </li>`).append($('<a href="#">[Delete]</a>')
+            .click(function () {
+                $.ajax({
+                    method: 'DELETE',
+                    url: URL + '/' + name + '.json'
+                }).then(() => $(li).remove())
+                    .catch(loadError);
+            }));
+        $('#phonebook').append(li)
+    }
+    $('#person').val('');
+    $('#phone').val('');
+}
+
+
+
+function loadError(err) {
+    console.log(err);
+}
